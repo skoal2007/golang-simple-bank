af95905c09573de3f088817632dbd71af2fb1435
heyee <heyee@heyees-MacBook-Pro.local>
2022-04-06 15:04:37 -0700

    11. Implement login user API that returns PASETO or JWT access token in Go

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,24 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	db "github.com/skoal2007/golang-simple-bank/db/sqlc"
+	"github.com/skoal2007/golang-simple-bank/util"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+	return server
+}
 
 func TestMain(m *testing.M) {
 
